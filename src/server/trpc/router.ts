b1e354be9ba502ab683a34b8864b7c7fa56323f7
liqiang <1206779266@qq.com>
2026-03-01 00:08:14 +0800

    feat(03-02): register conversationRouter in tRPC root
    - Import conversationRouter from ./routers/conversation
    - Add conversation: conversationRouter to appRouter

--- src/server/trpc/router.ts
+++ src/server/trpc/router.ts
@@ -2,11 +2,13 @@ import { createTRPCRouter } from './init'
 import { authRouter } from './routers/auth'
 import { userRouter } from './routers/user'
 import { requirementRouter } from './routers/requirement'
+import { conversationRouter } from './routers/conversation'
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   user: userRouter,
   requirement: requirementRouter,
+  conversation: conversationRouter,
 })
 
 export type AppRouter = typeof appRouter
